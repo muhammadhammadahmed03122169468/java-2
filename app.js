26a709cd8e02e1e25ad91f00bcb2c58ddfb770ec
muhammadhammadahmed03122169468 <hammad03122169468@gmail.com>
2024-02-04 15:39:43 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var fullName="Hafiz Muhammad Hammad Ahmed"
+alert(fullName)
+var age="20 year "
+alert(age)
+var Certified="Certified mobile application development"
+alert(Certified)
+
+
+
+
+var a=("pizza");
+var b=("pizz");
+var c=("piz");
+var d=("pi");
+var e=("p");
+
+alert(a+"\n"+b+"\n"+c+"\n"+d+"\n"+e);
+
+var email=prompt("Enter your email");
+var exp="hammad@email.com";
+
+alert(email +" "+ exp)
+
+var book=prompt("Book name");
+var review="A smarter way learn to java script";
+
+alert(book +" "+ review)
+
+document.write("Yeh! I can write HTML content through javascript")
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
